74cb9b8c6fcc1274630ebde9cb779d7dd67da6a6
Zelin <wangzl1116@mails.jlu.edu.cn>
2017-12-27 10:37:40 +0800

    解决link错误

--- AirTicketMap/src/AirTicSystem.cpp
+++ AirTicketMap/src/AirTicSystem.cpp
@@ -5,42 +5,10 @@
 #include<fstream>
 
 using namespace std;
-//非类中函数写到Common里
-
-
-Flight Create_New_Flight()
-{
-	string Line;
-	cout << "起始城市0,起始城市缩写1,到达城市2,到达城市缩写3,公司4,航班号5,起飞机场6,到达机场7,起飞时间8,"
-		<< "到达时间10,机型12,日期13,价格14,最大折扣15,满载16,当前人数17" << endl
-		<< "中间用逗号分隔" << endl;
-	cin >> Line;
-	vector<string> Line_Vec, Date_Vec, Time_Vec;
-	string sep = ",";
-	Split_Row(Line, sep, Line_Vec);
-
-
-	//日期
-	sep = "/";
-	Split_Row(Line_Vec[11], sep, Date_Vec);
-
-	string T_Stamp, L_Stamp;
-	sep = ":";
-	Split_Row(Line_Vec[8], sep, Time_Vec);
-	T_Stamp = Date_Vec[0] + Date_Vec[1] + Date_Vec[2] + Time_Vec[0] + Time_Vec[1];
-
-	Split_Row(Line_Vec[9], sep, Time_Vec);
-	L_Stamp = Date_Vec[0] + Date_Vec[1] + Date_Vec[2] + Time_Vec[0] + Time_Vec[1];
-
-
-	Line_Vec.insert(Line_Vec.begin() + 9, T_Stamp);
-	Line_Vec.insert(Line_Vec.begin() + 11, L_Stamp);
+//非类中函数写到Common.cpp里
 
 
 
-	Flight NN = Create_New_Flight(Line_Vec);
-	return NN;
-}
 	
 
 
